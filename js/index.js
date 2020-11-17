bb203bf95f37476b7b941de5fc0834c1c304675d
unknown <zhenyadope@gmail.com>
2020-11-17 22:58:57 +0300

    Добавлен слайдер

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 console.log(`Thanks for using my build ^-^`);
+
+const screenWidth = document.body.clientWidth;
+const sliderViewItems = 3;
+if( screenWidth < 1191){
+  sliderViewItems = 1;
+}
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView:3,
+    autoplay:true,
+    spaceBetween: 10,
+    freeMode: true,
+    // loop:true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
